bb0356bf7757cf1349155eac7f942ec94400e4bc
bluevice <wizardblue84@gmail.com>
2024-10-20 23:30:05 +0530

    changed the database

--- exit_emp/settings.py
+++ exit_emp/settings.py
@@ -107,14 +107,19 @@ WSGI_APPLICATION = 'exit_emp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PWD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+        'OPTIONS': {
+            'sslmode': os.getenv('SSL_MODE', 'require'),
+        }
     }
 }
 
 
-database_url=os.environ.get("DATABASE_URL")
-DATABASES["default"]=dj_database_url.parse(database_url)
 
 
 
