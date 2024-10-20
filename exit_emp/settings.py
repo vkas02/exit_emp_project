48504defc9a8b60a430b512c705ca2549cead1e7
bluevice <wizardblue84@gmail.com>
2024-10-20 23:43:39 +0530

    security key updated

--- exit_emp/settings.py
+++ exit_emp/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = "*88fo-8nm)gnq+vh1z4(s%%tt)hgr^rjdw7$vpzjw0x8fjh+!q"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG","False").lower()=="true"
