d422ff5a388bdcb39fc7a94d788f64870ceb765e
bluevice <wizardblue84@gmail.com>
2024-10-25 10:47:01 +0530

    improved EmployeeTaskSerializer

--- api/serializers.py
+++ api/serializers.py
@@ -24,10 +24,15 @@ class TaskSerializer(serializers.ModelSerializer):
 class EmployeeTaskSerializer(serializers.ModelSerializer):
     task_name=serializers.CharField(source='task.name',read_only=True)
     department_name=serializers.CharField(source='task.departments.name',read_only=True)
-    department_hod=serializers.CharField(source='task.departments.hod.first_name',read_only=True)
+    department_hod=serializers.SerializerMethodField()
     class Meta:
         model=EmployeeTask
         fields = ['id','status','task_name','department_name','department_hod']
+
+    def get_department_hod(self, obj):
+        if obj.task.departments.hod:
+            return f"{obj.task.departments.hod.first_name} {obj.task.departments.hod.last_name}"
+        return "No HoD assigned"
 
 
 class FeedbackQuestionsSerializer(serializers.ModelSerializer):
