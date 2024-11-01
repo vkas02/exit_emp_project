b009d90ac943529acf540b14e6ab15a80d356d2c
bluevice <wizardblue84@gmail.com>
2024-11-01 16:39:52 +0530

    improved EmployeeTask serializer

--- api/serializers.py
+++ api/serializers.py
@@ -25,9 +25,10 @@ class EmployeeTaskSerializer(serializers.ModelSerializer):
     task_name=serializers.CharField(source='task.name',read_only=True)
     department_name=serializers.CharField(source='task.departments.name',read_only=True)
     department_hod=serializers.SerializerMethodField()
+    employee_name=serializers.CharField(source='employee.name',read_only=True)
     class Meta:
         model=EmployeeTask
-        fields = ['id','status','task_name','department_name','department_hod']
+        fields = ['id','employee_name','status','task_name',]
 
     def get_department_hod(self, obj):
         if obj.task.departments.hod:
