ffde9838d348ae0ec9393acd2f8cece4793a8dc7
vkas02 <140232820+vkas02@users.noreply.github.com>
2024-11-27 15:29:44 +0530

    Update settings.py

--- exit_emp/settings.py
+++ exit_emp/settings.py
@@ -63,7 +63,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=300),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
 }
 
